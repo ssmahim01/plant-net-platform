e5b57661fc6b15482a8d11f0393f41f9201cd804
ssmahim01 <mahimzayn666@gmail.com>
2025-01-04 02:21:52 +0600

    Updated PurchaseModal component with PlantDetails page

--- client/src/components/Modal/PurchaseModal.jsx
+++ client/src/components/Modal/PurchaseModal.jsx
@@ -5,68 +5,155 @@ import {
   TransitionChild,
   DialogPanel,
   DialogTitle,
-} from '@headlessui/react'
-import { Fragment } from 'react'
+} from "@headlessui/react";
+import { Fragment, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
-const PurchaseModal = ({ closeModal, isOpen }) => {
-  // Total Price Calculation
+const PurchaseModal = ({ closeModal, isOpen, refetch, plant }) => {
+  const { _id, name, category, price, quantity, uploader } = plant || {};
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const axiosSecure = useAxiosSecure();
+  const [totalQuantity, setTotalQuantity] = useState(1);
+  const [totalPrice, setTotalPrice] = useState(price);
+  const [purchase, setPurchase] = useState({
+    customer: {
+      name: user?.displayName,
+      email: user?.email,
+      image: user?.photoURL,
+    },
+    plantId: _id,
+    price: totalPrice,
+    quantity: totalQuantity,
+    seller: uploader?.email,
+    address: "",
+    status: "Pending",
+  });
+
+  const handleQuantity = (value) => {
+    if (value > quantity) {
+      setTotalQuantity(quantity);
+      return toast.error("Quantity exceeds available stock!");
+    }
+    if (value < 0) {
+      setTotalQuantity(1);
+      return toast.error("Quantity cannot be less than 1");
+    }
+
+    setTotalQuantity(value);
+    setTotalPrice(value * price);
+    setPurchase((prev) => {
+      return { ...prev, quantity: value, price: value * price };
+    });
+  };
+
+  const handlePurchase = async () => {
+    console.table(purchase);
+  };
 
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as='div' className='relative z-10' onClose={closeModal}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
         <TransitionChild
           as={Fragment}
-          enter='ease-out duration-300'
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='ease-in duration-200'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <div className='fixed inset-0 bg-black bg-opacity-25' />
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
         </TransitionChild>
 
-        <div className='fixed inset-0 overflow-y-auto'>
-          <div className='flex min-h-full items-center justify-center p-4 text-center'>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
             <TransitionChild
               as={Fragment}
-              enter='ease-out duration-300'
-              enterFrom='opacity-0 scale-95'
-              enterTo='opacity-100 scale-100'
-              leave='ease-in duration-200'
-              leaveFrom='opacity-100 scale-100'
-              leaveTo='opacity-0 scale-95'
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
             >
-              <DialogPanel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+              <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <DialogTitle
-                  as='h3'
-                  className='text-lg font-medium text-center leading-6 text-gray-900'
+                  as="h3"
+                  className="text-lg font-medium text-center leading-6 text-gray-900"
                 >
                   Review Info Before Purchase
                 </DialogTitle>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Plant: Money Plant</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">Plant: {name}</p>
                 </div>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Category: Indoor</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">Category: {category}</p>
                 </div>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Customer: PH</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Customer: {user?.displayName}
+                  </p>
                 </div>
 
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Price: $ 120</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">Price: $ {price}</p>
                 </div>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Available Quantity: 5</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Available Quantity: {quantity}
+                  </p>
                 </div>
+
+                <div className="my-3 space-y-2">
+                  <div className="space-x-2 text-sm">
+                    <label htmlFor="quantity" className="text-gray-600">
+                      Quantity:
+                    </label>
+                    <input
+                      id="quantity"
+                      className="p-3 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white"
+                      name="quantity"
+                      value={totalQuantity}
+                      onChange={(e) => handleQuantity(parseInt(e.target.value))}
+                      type="number"
+                      placeholder="Provide your Quantity"
+                      required
+                    />
+                  </div>
+
+                  <div className="space-x-2 text-sm">
+                    <label htmlFor="address" className="text-gray-600">
+                      Address:
+                    </label>
+                    <input
+                      className="p-3 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white"
+                      name="address"
+                      onChange={(e) =>
+                        setPurchase((prev) => {
+                          return { ...prev, address: e.target.value };
+                        })
+                      }
+                      id="address"
+                      type="text"
+                      placeholder="Address"
+                      required
+                    />
+                  </div>
+                </div>
+                  <div className="mt-5">
+                    <button onClick={handlePurchase} className="w-full btn bg-green-500 border-none text-lg px-6 py-1 text-white font-bold rounded-full">{`Pay ${totalPrice}$`}</button>
+                  </div>
               </DialogPanel>
             </TransitionChild>
           </div>
         </div>
       </Dialog>
     </Transition>
-  )
-}
+  );
+};
 
-export default PurchaseModal
+export default PurchaseModal;
--- client/src/pages/PlantDetails/PlantDetails.jsx
+++ client/src/pages/PlantDetails/PlantDetails.jsx
@@ -1,29 +1,39 @@
-import Container from '../../components/Shared/Container'
-import { Helmet } from 'react-helmet-async'
-import Heading from '../../components/Shared/Heading'
-import Button from '../../components/Shared/Button/Button'
-import PurchaseModal from '../../components/Modal/PurchaseModal'
-import { useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
+import Container from "../../components/Shared/Container";
+import { Helmet } from "react-helmet-async";
+import Heading from "../../components/Shared/Heading";
+import Button from "../../components/Shared/Button/Button";
+import PurchaseModal from "../../components/Modal/PurchaseModal";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import LoadingSpinner from "../../components/Shared/LoadingSpinner";
 
 const PlantDetails = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   // console.log(id);
-  let [isOpen, setIsOpen] = useState(false)
+
+  const {
+    data: plant = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["plant", id],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_API_URL}/plant/${id}`
+      );
+      return data;
+    },
+  });
+
+  let [isOpen, setIsOpen] = useState(false);
 
   const closeModal = () => {
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
 
-  const {data:plant} = useQuery({
-    queryKey: ['plant', id],
-    queryFn: async() => {
-      const {data} = await axios.get(`${import.meta.env.VITE_API_URL}/plant/${id}`);
-      return data;
-    } 
-  });
+  if (isLoading) return <LoadingSpinner />;
 
   // console.log(plant);
 
@@ -32,88 +42,93 @@ const PlantDetails = () => {
       <Helmet>
         <title>{`PlantNet | ${plant?.name}`}</title>
       </Helmet>
-      <div className='mx-auto flex flex-col lg:flex-row justify-between w-full gap-12'>
+      <div className="mx-auto flex flex-col lg:flex-row justify-between w-full gap-12">
         {/* Header */}
-        <div className='flex flex-col gap-6 flex-1'>
+        <div className="flex flex-col gap-6 flex-1">
           <div>
-            <div className='w-full overflow-hidden rounded-xl'>
+            <div className="w-full overflow-hidden rounded-xl">
               <img
-                className='object-cover w-full'
+                className="object-cover w-full"
                 src={plant?.image}
                 alt={`Image of ${plant?.name}`}
               />
             </div>
           </div>
         </div>
-        <div className='md:gap-10 flex-1'>
+        <div className="md:gap-10 flex-1">
           {/* Plant Info */}
           <Heading
             title={plant?.name}
             subtitle={`Category: ${plant?.category}`}
           />
-          <hr className='my-6' />
+          <hr className="my-6" />
           <div
-            className='
-          text-lg font-light text-neutral-500'
+            className="
+          text-lg font-light text-neutral-500"
           >
-           {plant?.description}
+            {plant?.description}
           </div>
-          <hr className='my-6' />
+          <hr className="my-6" />
 
           <div
-            className='
+            className="
                 text-xl 
                 font-semibold 
                 flex 
                 flex-row 
                 items-center
                 gap-2
-              '
+              "
           >
             <div>Seller: {plant?.uploader?.name}</div>
 
             <img
-              className='rounded-full'
-              height='30'
-              width='30'
-              alt='Avatar'
-              referrerPolicy='no-referrer'
+              className="rounded-full"
+              height="30"
+              width="30"
+              alt="Avatar"
+              referrerPolicy="no-referrer"
               src={plant?.uploader?.image}
             />
           </div>
-          <hr className='my-6' />
+          <hr className="my-6" />
           <div>
             <p
-              className='
+              className="
                 gap-4 
                 font-light
                 text-neutral-500
-              '
+              "
             >
               Quantity: {plant?.quantity} Units Left Only!
             </p>
           </div>
-          <hr className='my-6' />
-          <div className='flex justify-between'>
-            <p className='font-bold text-3xl text-gray-500'>Price: {plant?.price}$</p>
+          <hr className="my-6" />
+          <div className="flex justify-between">
+            <p className="font-bold text-3xl text-gray-500">
+              Price: {plant?.price}$
+            </p>
             <div>
-              <Button onClick={() => {
-                setIsOpen(true);
-              }} label='Purchase' />
+              <Button
+                onClick={() => {
+                  setIsOpen(true);
+                }}
+                label={`${plant?.quantity > 0 ? "Purchase" : "Out of Stock"}`}
+              />
             </div>
           </div>
-          <hr className='my-6' />
+          <hr className="my-6" />
 
-          <PurchaseModal closeModal={closeModal} isOpen={isOpen} />
+          <PurchaseModal plant={plant} refetch={refetch} closeModal={closeModal} isOpen={isOpen} />
 
-          <div className='md:col-span-3 order-first md:order-last mb-10'>
+          <div className="md:col-span-3 order-first md:order-last mb-10">
             {/* RoomReservation */}
             {/* <RoomReservation room={room} /> */}
           </div>
         </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default PlantDetails
+export default PlantDetails;
